15ed5ba61c9a8f98657e558e2fd29f5acab01806
emersondms <emersondms@gmail.com>
2022-04-04 13:28:14 -0300

    test

--- send_order.py
+++ send_order.py
@@ -1,9 +1,9 @@
-def buy(mt5, stock, lots):
+def buy_at_market(mt5, stock, lots):
     request = {
         "symbol": stock, 
         "volume": lots,
-        "action": mt5.TRADE_ACTION_DEAL, #at market
         "type": mt5.ORDER_TYPE_BUY,
+        "action": mt5.TRADE_ACTION_DEAL, #at market
         "type_time": mt5.ORDER_TIME_DAY,
         "type_filling": mt5.ORDER_FILLING_RETURN
     }
@@ -11,19 +11,19 @@ def buy(mt5, stock, lots):
     try:
         print(f"\n>>> BUY order sent: {request}")
         result = mt5.order_send(request) 
-        print(f"    SUCCESS: {result}")
+        print(f"    RESULT: {result}")
         return result.order
     except:
         print(f"    FAILED: {mt5.last_error()}")
     return ""
 
-def sell(mt5, stock, lots, price):
+def sell_limit(mt5, stock, lots, price):
     request = { 
         "symbol": stock, 
         "volume": lots,
         "price": price,
+        "type": mt5.ORDER_TYPE_SELL_LIMIT,
         "action": mt5.TRADE_ACTION_PENDING,
-        "type": mt5.ORDER_TYPE_SELL,
         "type_time": mt5.ORDER_TIME_DAY,
         "type_filling": mt5.ORDER_FILLING_RETURN
     }
@@ -31,7 +31,7 @@ def sell(mt5, stock, lots, price):
     try:
         print(f"\n>>> SELL order sent: {request}")
         result = mt5.order_send(request) 
-        print(f"    SUCCESS: {result}")
+        print(f"    RESULT: {result}")
         return result.order
     except:
         print(f"    FAILED: {mt5.last_error()}")
--- test.py
+++ test.py
@@ -8,6 +8,7 @@ if not mt5.initialize():
     print("initialize() failed, error code = ", mt5.last_error())
     quit()
 
+
 stocks_dict = backtest_excel.get_stocks_dict(r"intersect_backtest.xlsx")
 good_stocks_rates_df = stocks_dataframe.get_good_stocks_rates(mt5, stocks_dict)
 
@@ -15,12 +16,33 @@ good_stocks_rates_df = stocks_dataframe.get_good_stocks_rates(mt5, stocks_dict)
 good_stocks_rates_df = good_stocks_rates_df.sort_values(
     by=['PROFIT_FACTOR', 'DIFF_FROM_CLOSE_TO_HALF'], ascending=False)
 
-
-print (good_stocks_rates_df)
+#print (good_stocks_rates_df)
 # print (good_stocks_df.loc['UNIP6'].name)
+#print(good_stocks_rates_df.iloc[0].name,"\n")
 
-#send_order.buy(mt5, "OIBR3", 100.0)
-#time.sleep(5)
-# send_order.sell(mt5, "OIBR3", 100.0, 0.83)
 
+positions=mt5.positions_get(symbol="MGLU3")
+if positions==None:
+    print("No positions on MGLU3, error code={}".format(mt5.last_error()))
+elif len(positions)>0:
+    print("Total positions on MGLU3 =",len(positions))
+    print(positions)
+else: 
+    print("No positions on MGLU3")
+
+
+
+
+
+
+'''
+lots = 100.0
+stock = "MGLU3"
+sell_price = 7.15
+
+
+send_order.buy_at_market(mt5, stock, lots)
+time.sleep(5)
+send_order.sell_limit(mt5, stock, lots, sell_price)
 #mt5.shutdown()
+'''
